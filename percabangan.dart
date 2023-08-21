cdb6fdfb10543f743c98ba1740a5e6ac8f46eeb3
mustika chairani <mustikachrn5@gmail.com>
2023-08-21 11:22:35 +0700

    Update percabangan.dart

--- percabangan.dart
+++ percabangan.dart
@@ -1,8 +1,27 @@
-int jumlah(int a, int b) {
+/*int jumlah(int a, int b) {
   return a + b;
 }
 
 void main() {
   var hasil = jumlah(1, 3);
+  print(hasil);
+}
+*/
+
+String ceknilai(int nilai) {
+  if (nilai > 70) {
+    return 'Nilai A';
+  } else if (nilai > 40) {
+    return 'Nilai B';
+  } else if (nilai > 0) {
+    return 'Nilai C';
+  } else {
+    return "";
+  }
+}
+
+void main() {
+  int nilaiMhs = 45;
+  String hasil = ceknilai(nilaiMhs);
   print(hasil);
 }
