024aaa2c99455d3f4a02fa96b1e9505d11c6eeb4
mustika chairani <mustikachrn5@gmail.com>
2023-08-30 11:12:11 +0700

    Update prio1.dart

--- prio1.dart
+++ prio1.dart
@@ -1,11 +1,20 @@
+Future<void> duaParameter(List<int> data, List<int> pengali) async {
+  print("Memulai proses data...");
 
-Future<List<int>> duaParameter(List<int>data, int pengali) async{
-  
+  await Future.delayed(Duration(seconds: 2));
+
+  int totalData = data.reduce((value, element) => value + element);
+  int totalPengali = pengali.reduce((value, element) => value + element);
+
+  print("Total data : $totalData");
+  print("Total pengali : $totalPengali");
+
+  print("Proses data selesai");
 }
 
 void main() {
-  int listdata = 3;
-  int pengali = 5;
-  int hasil = duaParameter(listdata, pengali);
-  print("hasil list $hasil");
+  List<int> data = [1, 2, 3, 4, 5];
+  List<int> pengali = [6, 7, 8, 9, 10];
+
+  duaParameter(data, pengali);
 }
